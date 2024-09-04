8c3357a28d850f618510cf52a70f941ce435a9ef
DiogoT25 <101741060+DiogoT25@users.noreply.github.com>
2024-09-04 18:35:00 +0100

    create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+
+# criar a interface do streamlit
+st.write("""
+# App Preço de Ações
+O gráfico abaixo representa a evolução do preço das ações do Itaú (ITUB4) ao longo do ano
+""") # markdown
