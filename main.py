e1f3895fdc34b15f27c6336bd22eac1bf1a31810
DiogoT25 <diogo.torneiro@mlanalytics.pt>
2024-09-05 18:11:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,25 @@ import pandas as pd
 import yfinance as yf
 
 # criar a interface do streamlit
+@st.cache_data
+def load_data(company):
+    dados_acao = yf.Ticker(company)
+    cotacoes_acao = dados_acao.history(period="1d", start="2010-01-01", end="2024-07-01")
+    cotacoes_acao = cotacoes_acao[["Close"]]
+    return cotacoes_acao
+
+dados = load_data("ITUB4.SA")
+print(dados)
+
 st.write("""
-# App Preço de Ações
-O gráfico abaixo representa a evolução do preço das ações do Itaú (ITUB4) ao longo do ano
+# Stock Prices
+This graph highlights the stock price evolution over the time
 """) # markdown
+
+# create line chart
+st.line_chart(dados)
 
 # criar a interface do streamlit
 st.write("""
-# Fim do App ATUALIZAÇÃO sjjdfdgg
-    ddd tenta agora
+# Fim do App
 """) # markdown
