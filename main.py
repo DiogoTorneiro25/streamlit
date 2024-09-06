159620dbbced3996ac498aa30438bf164a1c79f3
DiogoT25 <diogo.torneiro@mlanalytics.pt>
2024-09-06 11:02:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,28 @@ import streamlit as st
 # create a function to load data
 @st.cache_data
 def load_data(companies):
-    tickers_text = " ".join(companies)
-    dados_acao = yf.Ticker(tickers_text)
-    cotacoes_acao = dados_acao.history(period="1d", start="2010-01-01", end="2024-07-01")
-    print(cotacoes_acao)
-    cotacoes_acao = cotacoes_acao[["Close"]]
-    return cotacoes_acao
+    data = pd.DataFrame()
+    for company in companies:
+        #tick_text = " ".join(company)
+        dados_acao = yf.Ticker(company)
+        cotacoes_acao = dados_acao.history(period="1d", start="2016-01-01", end="2024-07-01")
+    #print(cotacoes_acao)
+        cotacoes_acao = cotacoes_acao["Close"].rename(company)
+        data = pd.concat([data, cotacoes_acao], axis=1)
+    return data
 
 stocks = ["ITUB4.SA", "PETR4.SA", "MGLU3.SA", "VALE3.SA", "ABEV3.SA", "GGBR4.SA"]
-
-# prepare visualizations
 data = load_data(stocks)
-print(data)
+
+# prepare visualizations and filters
+stocks_list = st.multiselect("Choose the stocks to evaluate",data.columns)
+if stocks_list:
+    data = data[stocks_list]
+    if len(stocks_list) == 1:
+        single_stock = stocks_list[0]
+        data = data.rename(columns={single_stock: "Close"})
+        
+#print(lista_acoes)
 
 st.write("""
 # Stock Prices
@@ -26,9 +36,9 @@ This graph highlights the stock price evolution over the time
 """) # markdown
 
 # create line chart
-st.line_chart(dados)
+st.line_chart(data)
 
 # criar a interface do streamlit
 st.write("""
-# Fim do App
+# End of App
 """) # markdown
